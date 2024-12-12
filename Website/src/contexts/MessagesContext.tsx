c26af821756f6e7dad924d44d6eb84260bd1160c
jkrue242 <josephkrueger242@gmail.com>
2024-12-12 17:39:20 -0600

    add linter script

--- Website/src/contexts/MessagesContext.tsx
+++ Website/src/contexts/MessagesContext.tsx
@@ -1,9 +1,4 @@
-import React, {
-  createContext,
-  useContext,
-  useState,
-  useEffect,
-} from "react";
+import React, { createContext, useContext, useState, useEffect } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import uuid from "react-native-uuid";
 import { Message, MessagesProviderProps } from "../types/types";
